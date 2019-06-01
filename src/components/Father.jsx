5703343e3e00ab84fe69dcb95f90526abedfb9a6
Augusto Scher <augustoscher@gmail.com>
2019-06-01 06:33:39 -0300

    25 - comunicação indireta entre components

--- src/components/Father.jsx
+++ src/components/Father.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import Son from './Son'
 
 export default props => {
-    const action = place => {
-        alert(`You can go to the ${place}`)
+    const actionFunction = param => {
+        alert(`You can go to the ${param}`)
     }
     
     return (
-        <Son notifyButton={action}></Son>
+        <Son notifyButton={actionFunction}></Son>
     )
 }
--- src/components/Son.jsx
+++ src/components/Son.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 
 export default props => 
   <div>
+    {/* calls the function passed by props.
+      it will pass 'beach' as param of the function.
+    */}
     <button onClick={() => props.notifyButton('beach')}>
       Going out
     </button>
