32309856ab3cd2a3210ba676e56f3e83cbd18193
Augusto S. Scher <augusto@jusbrasil.com.br>
2022-02-28 10:47:03 -0300

    feat: add FetchItems component

--- src/components/FetchItems/index.tsx
+++ src/components/FetchItems/index.tsx
@@ -13,26 +13,52 @@ type Data = {
 
 const FetchItems = () => {
   const [items, setItems] = useState<Item[]>([])
+  const [query, setQuery] = useState('redux')
+  const [isLoading, setIsLoading] = useState(false)
+  const [isError, setIsError] = useState(false)
 
   useEffect(() => {
     const fetchData = async () => {
-      const { data } = await axios.get<Data>(
-        'https://hn.algolia.com/api/v1/search?query=redux'
-      )
-      setItems(data.hits)
+      setIsLoading(true)
+      setIsError(false)
+
+      try {
+        const { data } = await axios.get<Data>(
+          `https://hn.algolia.com/api/v1/search?query=${query}`
+        )
+        setItems(data.hits)
+      } catch (error) {
+        setIsError(true)
+      }
+
+      setIsLoading(false)
     }
 
     fetchData()
-  }, [])
+  }, [query])
 
   return (
-    <ul>
-      {items.map((item) => (
-        <li key={item.objectID}>
-          <a href={item.url}>{item.title}</a>
-        </li>
-      ))}
-    </ul>
+    <>
+      <input
+        type="text"
+        value={query}
+        onChange={(event) => setQuery(event.target.value)}
+      />
+
+      {isError && <div>Something went wrong ...</div>}
+
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : (
+        <ul>
+          {items.map((item) => (
+            <li key={item.objectID}>
+              <a href={item.url}>{item.title}</a>
+            </li>
+          ))}
+        </ul>
+      )}
+    </>
   )
 }
 
