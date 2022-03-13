6319547d033970c259650b04ae9efbb2b0df885d
Augusto S. Scher <augusto@jusbrasil.com.br>
2022-03-12 22:08:04 -0300

    feat: badge component

--- src/components/Badge/styles.ts
+++ src/components/Badge/styles.ts
@@ -3,12 +3,12 @@ import styled from 'styled-components'
 export const Wrapper = styled.div`
   display: inline-block;
   padding: 4px;
-  margin: 0 4px;
   color: rgb(54, 58, 69);
   background-color: rgb(199, 206, 212);
-  font-weight: 700;
+  font-weight: 600;
   line-height: 1;
   text-align: center;
   white-space: nowrap;
   border-radius: 2px;
+  margin: 0 4px 0 0;
 `
