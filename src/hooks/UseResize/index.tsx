5635d1a0c736150b6ef6747d9a9eab640b17eca6
Augusto S. Scher <augusto@jusbrasil.com.br>
2022-04-06 00:25:30 -0300

    feat: useResize

---
+++ src/hooks/UseResize/index.tsx
@@ -0,0 +1,24 @@
+import useMouse from 'hooks/UseMouse'
+import { useEffect, useState } from 'react'
+
+const useResize = (initialSize = {}) => {
+  const [size, setSize] = useState(initialSize)
+
+  const handleResize = () => {
+    setSize({
+      width: window.innerWidth,
+      height: window.innerHeight
+    })
+  }
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize)
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [])
+
+  return { size }
+}
+
+export default useResize
