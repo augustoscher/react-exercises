671b370cd4f4144e22617ea3678d847af32bb150
Augusto S. Scher <augusto@jusbrasil.com.br>
2022-04-14 12:01:30 -0300

    feat: Heavy component

---
+++ src/components/Heavy/index.tsx
@@ -0,0 +1,26 @@
+import { memo } from 'react'
+
+// eslint-disable-next-line react/display-name
+export default memo(() => (
+  <>
+    <h1>1. Title</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut quos quia
+      mollitia vitae et, possimus aspernatur. Eum reiciendis cum provident
+      ratione magnam, non iste cumque recusandae autem assumenda reprehenderit
+      nesciunt.
+    </p>
+    <h2>1.1 Subtitle</h2>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et neque commodi
+      illo sequi quasi eligendi consectetur fugiat sed nisi iusto ratione
+      maiores iure, placeat aspernatur aliquam odio saepe. Ab, assumenda.
+    </p>
+    <h2>1.2 Subtitle</h2>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et neque commodi
+      illo sequi quasi eligendi consectetur fugiat sed nisi iusto ratione
+      maiores iure, placeat aspernatur aliquam odio saepe. Ab, assumenda.
+    </p>
+  </>
+))
