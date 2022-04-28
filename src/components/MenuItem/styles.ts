910ea270e1a482ec07774350518e0c09505d503c
Augusto S. Scher <augusto@jusbrasil.com.br>
2022-04-28 01:18:40 -0300

    feat: link animation

--- src/components/MenuItem/styles.ts
+++ src/components/MenuItem/styles.ts
@@ -1,5 +1,29 @@
 import styled from 'styled-components'
 
 export const Wrapper = styled.div`
-  margin: 2px 0;
+  width: 100%;
+
+  a {
+    text-decoration: none;
+    color: #171616;
+    transform: translateX(-0.75rem);
+    transition: transform 200ms;
+  }
+
+  a::before {
+    content: '#';
+    display: inline-block;
+    width: 0.75rem;
+    color: #888888;
+    opacity: 0;
+    transition: opacity 200ms;
+  }
+
+  a:hover {
+    transform: none;
+  }
+
+  a:hover::before {
+    opacity: 1;
+  }
 `
