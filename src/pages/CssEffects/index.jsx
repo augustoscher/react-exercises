22ce563925f2359431180c121ac886a4d521238d
Augusto S. Scher <augusto@jusbrasil.com.br>
2022-04-29 01:44:53 -0300

    feat: add Image Card Resizable on CssEffects page

--- src/pages/CssEffects/index.jsx
+++ src/pages/CssEffects/index.jsx
@@ -1,5 +1,6 @@
 import Base from 'templates/Base'
 import ExampleDescription from 'components/ExampleDescription'
+import ImageCard from 'components/ImageCard'
 
 const CssEffectsPage = () => (
   <Base>
@@ -9,6 +10,19 @@ const CssEffectsPage = () => (
       tags={['styled components', 'css']}
     />
 
+    <hr />
+
+    <ImageCard
+      title="Camera"
+      src="https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8Zm9jdXN8ZW58MHx8MHx8&w=1000&q=80"
+      alt="camera"
+    />
+
+    <ImageCard
+      title="Trees"
+      src="https://www.industrialempathy.com/img/remote/ZiClJf-1920w.jpg"
+      alt="trees"
+    />
     <hr />
   </Base>
 )
