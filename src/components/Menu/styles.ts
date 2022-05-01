3a2d61e6cdd37d2181ca802448aba3f3a0f5e92a
Augusto S. Scher <augusto@jusbrasil.com.br>
2022-05-01 14:07:41 -0300

    feat: menu sticky

--- src/components/Menu/styles.ts
+++ src/components/Menu/styles.ts
@@ -2,6 +2,12 @@ import styled from 'styled-components'
 
 export const Wrapper = styled.nav`
   padding: 1.2rem;
+  box-shadow: 0 1px 2px hsla(0, 0%, 0%, 0.05), 0 1px 4px hsla(0, 0%, 0%, 0.05),
+    0 2px 8px hsla(0, 0%, 0%, 0.05);
+  position: sticky;
+  top: 0;
+  z-index: 1;
+  background-color: white;
 `
 
 export const List = styled.ul`
