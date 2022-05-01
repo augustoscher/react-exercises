9a1b0011bf2be6ee4b84017d0730f6180b1ce29f
Augusto S. Scher <augusto@jusbrasil.com.br>
2022-05-01 14:17:10 -0300

    feat: add header element

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -8,13 +8,15 @@ type MenuProps = {
 
 const Menu = ({ items }: MenuProps) => (
   <S.Wrapper>
-    <S.List>
-      {items.map((item, idx) => (
-        <S.ListItem key={idx}>
-          <MenuItem {...item} />
-        </S.ListItem>
-      ))}
-    </S.List>
+    <nav>
+      <S.List>
+        {items.map((item, idx) => (
+          <S.ListItem key={idx}>
+            <MenuItem {...item} />
+          </S.ListItem>
+        ))}
+      </S.List>
+    </nav>
   </S.Wrapper>
 )
 
--- src/components/Menu/styles.ts
+++ src/components/Menu/styles.ts
@@ -1,6 +1,6 @@
 import styled from 'styled-components'
 
-export const Wrapper = styled.nav`
+export const Wrapper = styled.header`
   padding: 1.2rem;
   box-shadow: 0 1px 2px hsla(0, 0%, 0%, 0.05), 0 1px 4px hsla(0, 0%, 0%, 0.05),
     0 2px 8px hsla(0, 0%, 0%, 0.05);
