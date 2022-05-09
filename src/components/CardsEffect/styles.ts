ac9980d242a42abf5f62cac6851f04dc6499e017
Augusto S. Scher <augusto@jusbrasil.com.br>
2022-05-08 23:12:35 -0300

    feat: CardEffects

---
+++ src/components/CardsEffect/styles.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.div`
+  display: grid;
+  padding: 0.8rem 0;
+  grid-template-columns: repeat(auto-fit, minmax(150px, 1fr));
+  grid-gap: 0.8rem;
+  /* overflow: auto;
+  resize: both; */
+  border: 1px solid #888888;
+  border-radius: 0.2rem;
+`
