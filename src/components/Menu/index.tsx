1d10f0e17e80b65a6d5f24c276935dee02b48555
Augusto S. Scher <augusto@jusbrasil.com.br>
2022-05-13 01:12:39 -0300

    feat: new menu

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import MenuItem, { MenuItemProps } from '../MenuItem'
 
 import * as S from './styles'
@@ -6,18 +7,43 @@ type MenuProps = {
   items: MenuItemProps[]
 }
 
-const Menu = ({ items }: MenuProps) => (
-  <S.Wrapper>
-    <nav>
-      <S.List>
-        {items.map((item, idx) => (
-          <S.ListItem key={idx}>
-            <MenuItem {...item} />
-          </S.ListItem>
-        ))}
-      </S.List>
-    </nav>
-  </S.Wrapper>
-)
+const Menu = ({ items }: MenuProps) => {
+  const [isOpenMenu, setOpenMenu] = useState(false)
+
+  const handleClick = () => {
+    console.log('isOpenMenu: ', isOpenMenu)
+    setOpenMenu(!isOpenMenu)
+  }
+
+  return (
+    <S.Wrapper>
+      <S.Nav>
+        <S.Container>
+          <S.Logo href="/">Home</S.Logo>
+        </S.Container>
+        <S.Menu isOpenMenu={isOpenMenu}>
+          <S.List>
+            {items.map((item, idx) => (
+              <S.ListItem key={idx}>
+                <MenuItem {...item} />
+              </S.ListItem>
+            ))}
+          </S.List>
+        </S.Menu>
+        <S.Container>
+          <S.BurgerButton
+            role="button"
+            onClick={handleClick}
+            menuOpened={isOpenMenu}
+          >
+            <span className="line line1"></span>
+            <span className="line line2"></span>
+            <span className="line line3"></span>
+          </S.BurgerButton>
+        </S.Container>
+      </S.Nav>
+    </S.Wrapper>
+  )
+}
 
 export default Menu
--- src/components/Menu/styles.ts
+++ src/components/Menu/styles.ts
@@ -1,23 +1,111 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
+import media from 'styled-media-query'
 
-export const Wrapper = styled.header`
-  padding: 1.2rem;
+export const Wrapper = styled.header``
+
+export const Nav = styled.nav`
+  display: flex;
   box-shadow: 0 1px 2px hsla(0, 0%, 0%, 0.05), 0 1px 4px hsla(0, 0%, 0%, 0.05),
     0 2px 8px hsla(0, 0%, 0%, 0.05);
-  position: sticky;
-  top: 0;
-  z-index: 1;
   background-color: white;
+  padding: 1.2rem;
+  justify-content: space-between;
+
+  ${media.greaterThan('medium')`
+    align-items: center;
+  `};
+`
+
+export const Container = styled.div`
+  width: 60px;
+`
+
+export const Logo = styled.a`
+  padding: 0.4rem 2rem 0.4rem 0.4rem;
+  text-decoration: none;
+  cursor: pointer;
+
+  ${media.lessThan('medium')`
+    padding: 0.4rem;
+  `};
+`
+
+type MenuProps = {
+  isOpenMenu: boolean
+}
+
+const menuModifiers = {
+  closed: () => css`
+    max-height: 0;
+    overflow: hidden;
+  `,
+  opened: () => css`
+    transform: translateY(5%);
+    transition: transform 0.5s ease-in-out;
+  `
+}
+
+export const Menu = styled.div<MenuProps>`
+  ${({ isOpenMenu }) => css`
+    display: flex;
+    width: 100%;
+
+    ${media.lessThan('medium')`
+      ${isOpenMenu ? menuModifiers.opened : menuModifiers.closed}
+    `};
+  `}
 `
 
 export const List = styled.ul`
   display: flex;
+  flex-direction: row;
+  width: 100%;
   flex-wrap: wrap;
   list-style: none;
   margin: 0;
   padding: 0;
+
+  ${media.lessThan('medium')`
+    flex-direction: column;
+    align-items: center;
+    padding: 20px;
+  `};
 `
 
 export const ListItem = styled.li`
   padding: 0.4rem;
+`
+
+type BurgerButtonProps = {
+  menuOpened: boolean
+}
+
+const modifiers = {
+  collapsed: () => css`
+    background: red;
+  `,
+  normal: () => css`
+    background: #0e2431;
+  `
+}
+
+export const BurgerButton = styled.div<BurgerButtonProps>`
+  ${({ menuOpened }) => css`
+    ${media.lessThan('medium')`
+      height: 20px;
+      width: 32px;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      cursor: pointer;
+
+      span {
+        display: block;
+        height: 3px;
+        width: 100%;
+        border-radius: 10px;
+        ${menuOpened ? modifiers.normal : modifiers.collapsed}
+      }
+    `}
+  `}
 `
